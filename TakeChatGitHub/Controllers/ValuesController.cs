85c609c6e3881bce4d66db181b2ee9c801edee0b
Aline Camargos - TI - Engeman <aline.camargos@engecompany.com.br>
2021-10-27 10:08:26 -0300

    Initial Commit

---
+++ TakeChatGitHub/Controllers/ValuesController.cs
@@ -0,0 +1,27 @@
+﻿using Flurl.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using challenge.Models;
+
+namespace challenge.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class ValuesController : ControllerBase
+  {
+    [HttpGet]
+    public async Task<List<GitHubRepo>> Get()
+    {
+      IList<GitHubRepo> lista = await "https://api.github.com/users/takenet/repos?per_page=1000"
+        .WithBasicAuth("alinecamardev", "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")
+        .WithHeader("Accept", "application/vnd.github.v3+json")
+        .WithHeaders(new { User_Agent = "product/1" })
+        .GetJsonAsync<List<GitHubRepo>>();
+
+      return lista.Where(c => c.language == "C#").OrderBy(c => c.created_at).Take(5).ToList();
+    }
+
+  }
+}
---
+++ TakeChatGitHub/Models/GitHubRepo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace challenge.Models
+{
+  public class GitHubRepo
+  {
+    public string language { get; set; }
+    public string full_name { get; set; }
+    public string description { get; set; }
+    public DateTime created_at { get; set; }
+  }
+}
